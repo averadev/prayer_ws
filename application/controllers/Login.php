9fa18f7d4a503837a8afe6c8c36f287a0161fbe0
Faustino Loeza <faustinoloezaperez@gmail.com>
2016-12-21 12:48:22 -0500

    sessions

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,6 +5,7 @@ class Login extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
+		$this->load->library('Nativesessions');
 		$this->load->helper('url');
 		$this->load->database('default');
 		$this->load->model('Admins_model');
